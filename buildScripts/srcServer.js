8be548155ad75035fbd6aa2fcb408f823f12d144
Joe Bocking <joe.bocking@abdynamics.com>
2022-06-05 18:40:52 +0100

    http calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,8 +1,8 @@
-import path from "path";
-import open from "open";
-import express from "express";
-import webpack from "webpack";
-import config from "../webpack.config.dev";
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 /* eslint-disable no-console */
 
@@ -11,25 +11,26 @@ const app = express();
 const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-    publicPath: config.output.publicPath
+  publicPath: config.output.publicPath
 }));
 
-app.get('/users', function(req, res) {
-    res.json([
-        {"id": 1,"firstName":"Bob", "lastName" : "Smith", "email": "bob@gmail.com"},
-        {"id": 2,"firstName":"Tammy", "lastName" : "Norton", "email": "tnorton@gmail.com"},
-        {"id": 3,"firstName":"Tina", "lastName" : "Lee", "email": "lee.tina@gmail.com"}
-    ]);
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../src/index.html'))
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, (err) => {
-    if(err){
-        console.log(err);
-    } else{
-        open('http://localhost:' + port);
-    }
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
 });
